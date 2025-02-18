a4b8afa76eab32a3903da8d60925048fd08ef5a9
jckend <142367196+jckend@users.noreply.github.com>
2025-02-18 18:01:15 -0500

    Update experiment.ts

--- hosting/src/experiment.ts
+++ hosting/src/experiment.ts
@@ -114,72 +114,13 @@ export async function runExperiment(updateDebugPanel: () => void) {
 
    var trial_1 = {
       type: jsPsychHtmlSliderResponse,
-      stimulus: '3foihrfo',
+      stimulus: '<p>blah blah</p>',
       labels: ['Purple', 'Blue'],
       slider_width: 500,
       require_movement: true,
       prompt: '<p>Is this color closer to purple or blue? Use the slider above.</p>'
     }
-  
-  /* define welcome message trial */
-  const welcome = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: '<span class="text-xl">Welcome to the experiment. Press any key to begin.</span>',
-  }
-  timeline.push(welcome)
-
-    /* consent */
-  const consent = {
-    type: jsPsychHtmlButtonResponse,
-    stimulus: `
-    <div style="margin-left: 200px; margin-right: 200px; text-align: left;">
-      <b><p style="margin-bottom: 20px;">Please consider this information carefully before deciding whether to participate in this research.</p></b>
-      
-      <p style="margin-bottom: 20px;">The purpose of this research is to examine which factors influence linguistic meaning. You will be asked to make judgements about the meaning of sentences in different scenarios. We are simply interested in your judgement. The study will take less than 1 hour to complete, and you will receive less than $20 on Prolific. Your compensation and time commitment are specified in the study description. There are no anticipated risks associated with participating in this study. The effects of participating should be comparable to those you would ordinarily experience from viewing a computer monitor and using a mouse or keyboard for a similar amount of time. At the end of the study, we will provide an explanation of the questions that motivate this line of research and will describe the potential implications.</p>
-      
-      <p style="margin-bottom: 20px;"margin-bottom: 50px;>Your participation in this study is completely voluntary and you may refuse to participate or you may choose to withdraw at any time without penalty or loss of benefits to you which are otherwise entitled. Your participation in this study will remain confidential. No personally identifiable information will be associated with your data. Also, all analyses of the data will be averaged across all the participants, so your individual responses will never be specifically analyzed.</p>
-      
-      <p style="margin-bottom: 20px;">If you have questions or concerns about your participation or payment, or want to request a summary of research findings, please contact Dr. Jonathan Phillips at <a href="mailto:Jonathan.S.Phillips@dartmouth.edu">Jonathan.S.Phillips@dartmouth.edu</a>.</p>
-      
-      <p style="margin-bottom: 20px;">Please save a copy of this form for your records.</p>
-      
-      <h3><b>Agreement:</b></h3>
-      
-      <p>The nature and purpose of this research have been sufficiently explained and I agree to participate in this study. I understand that I am free to withdraw at any time without incurring any penalty. Please consent by clicking the button below to continue. Otherwise, please exit the study at any time.</p>
-    </div>
-  `,
-    choices: ['Submit'],
-    //this specifies the way in which the data will be configured inside jspsych data variable...
-    data: {
-      internal_type: 'consent',
-      trial_name: 'consent',
-    },
-  }
-  timeline.push(consent)
-
-  /* define instructions for main trial */
-  var instructions1 = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: `
-    <p>You will be presented with two images. Select the image you think is likelier to fit the description.</p>
-    <p>If the likelier image is on the left, press the left arrow <kbd>&larr;</kbd> on the keyboard as fast as you can. If the likelier image is on the right, press the right arrow <kbd>&rarr;</kbd> as fast as you can.</p>
-    <center>
-    <div style='width: 700px;'><img src='${imgSnail1}'></img>
-    </div>
-    </center>
-    <p>Press any key to begin.</p>
-  `,
-    post_trial_gap: 2000,
-  }
-  timeline.push(instructions1)
-
-  /* define test procedure */
-  const test_procedure = {
-    timeline: [trial_1],
-    repetitions: 1,
-    randomize_order: true,
-  }
-  timeline.push(test_procedure)
+    timeline.push(trial_1)
   
 
   /* start the experiment */
