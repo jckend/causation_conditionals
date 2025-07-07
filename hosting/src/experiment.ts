c2ed428c7464dae0293e423a135579ea54ad3230
jckend <142367196+jckend@users.noreply.github.com>
2025-07-07 13:21:14 -0400

    Update experiment.ts

--- hosting/src/experiment.ts
+++ hosting/src/experiment.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-
+import externalHtml from '@jspsych/plugin-external-html'
+import jsPsychHtmlButtonResponse from '@jspsych/plugin-html-button-response'
 import jsPsychHtmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response'
 import jsPsychHtmlSliderResponse from '@jspsych/plugin-html-slider-response'
-import jsPsychImageSliderResponse from '@jspsych/plugin-image-slider-response'
 import jsPsychImageKeyboardResponse from '@jspsych/plugin-image-keyboard-response'
 import jsPsychPreload from '@jspsych/plugin-preload'
 import { initJsPsych } from 'jspsych'
@@ -62,6 +62,7 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
 
   /* initialize jsPsych */
   const jsPsych = initJsPsych({
+    show_progress_bar: true,
     on_data_update: function (trialData: TrialData) {
       if (debug) {
         console.log('jsPsych-update :: trialData ::', trialData)
@@ -118,46 +119,79 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
   }
   timeline.push(welcome)
 
+    /* consent */
+  const consent = {
+    type: jsPsychHtmlButtonResponse,
+    stimulus: `
+    <div style="margin-left: 200px; margin-right: 200px; text-align: left;">
+      <b><p style="margin-bottom: 20px;">Please consider this information carefully before deciding whether to participate in this research.</p></b>
+      
+      <p style="margin-bottom: 20px;">The purpose of this research is to examine which factors influence linguistic meaning. You will be asked to make judgements about the meaning of sentences in different scenarios. We are simply interested in your judgement. The study will take less than 1 hour to complete, and you will receive less than $20 on Prolific. Your compensation and time commitment are specified in the study description. There are no anticipated risks associated with participating in this study. The effects of participating should be comparable to those you would ordinarily experience from viewing a computer monitor and using a mouse or keyboard for a similar amount of time. At the end of the study, we will provide an explanation of the questions that motivate this line of research and will describe the potential implications.</p>
+      
+      <p style="margin-bottom: 20px;"margin-bottom: 50px;>Your participation in this study is completely voluntary and you may refuse to participate or you may choose to withdraw at any time without penalty or loss of benefits to you which are otherwise entitled. Your participation in this study will remain confidential. No personally identifiable information will be associated with your data. Also, all analyses of the data will be averaged across all the participants, so your individual responses will never be specifically analyzed.</p>
+      
+      <p style="margin-bottom: 20px;">If you have questions or concerns about your participation or payment, or want to request a summary of research findings, please contact Dr. Jonathan Phillips at <a href="mailto:Jonathan.S.Phillips@dartmouth.edu">Jonathan.S.Phillips@dartmouth.edu</a>.</p>
+      
+      <p style="margin-bottom: 20px;">Please save a copy of this form for your records.</p>
+      
+      <h3><b>Agreement:</b></h3>
+      
+      <p>The nature and purpose of this research have been sufficiently explained and I agree to participate in this study. I understand that I am free to withdraw at any time without incurring any penalty. Please consent by clicking the button below to continue. Otherwise, please exit the study at any time.</p>
+    </div>
+  `,
+    choices: ['Submit'],
+    //this specifies the way in which the data will be configured inside jspsych data variable...
+    data: {
+      internal_type: 'consent',
+      trial_name: 'consent',
+    },
+  }
+  timeline.push(consent)
+
+
   /* define instructions trial */
   const instructions = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <p>In this experiment, you will be presented with an image and asked some questions.</p>
-    <p>Press any key to begin.</p>
+<p>In this experiment, you be given a description of a context.</p>
+<p>You will first be asked to make a judgment about the likelihood of an event.</p>
+<p>You will then be asked to judge how odd you find a sentence in the context.</p> 
+<p>In all examples, assume that the speaker is honest and has no incentive deceive you.</p>
+<p>Press any key to begin.</p>
     `,
     post_trial_gap: 2000,
   }
   timeline.push(instructions)
 
-  var ignorance_1 = {
-    type: jsPsychHtmlSliderResponse,
-    stimulus: 'There are two ways to get $5 from Mr. Johnson: mowing his lawn or cleaning his gutters. However, Laura believes that mowing Mr. Johnson’s lawn is the only way to obtain $5 from him. She tells you: “If you mow Mr. Johnson’s lawn, he’ll pay you $5.”',
-    stimulus_width: 700,
-    prompt: 'Do you think Laura would accept the following statement: “If you don’t mow Mr. Johnson’s lawn, he won’t pay you $5.”',
-    labels: ["no", "unsure", "yes"],
-    slider_width: 500,
-    require_movement: true, 
-    on_finish: function (data: TrialData) {
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, unicorn/no-null
-      data.saveIncrementally = true
-    },
-  }
-    timeline.push(ignorance1)
-
-var ignorance_2 = {
-    type: jsPsychHtmlSliderResponse,
-    stimulus: 'Bob has two risk factors for cardiovascular disease: he smokes and he drinks excessively. However, Bob has lied to his doctor about his drinking. Bob’s doctor tells you: “If Bob doesn’t quit smoking, he’ll get cardiovascular disease.”',
-    stimulus_width: 700,
-    prompt: 'Do you think Bob’s doctor would accept the following statement: “If Bob quits smoking, he won’t get cardiovascular disease.”', 
-    labels: ["no", "unsure", "yes"],
-    slider_width: 500,
-    require_movement: true, 
-    on_finish: function (data: TrialData) {
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, unicorn/no-null
-      data.saveIncrementally = true
-    },
-  }
+  /* define trial stimuli array for timeline variables */
+  const test_stimuli: Record<string, string>[] = [
+    { stimulus1: 'Bob has two risk factors for cardiovascular disease: he smokes and he drinks excessively. However, Bob has lied to his doctor about his drinking. Bob’s doctor tells you: “If Bob doesn’t quit smoking, he’ll get cardiovascular disease.”</p>', prompt1: '<p>Do you think Bob’s doctor would accept the following statement: “If Bob quits smoking, he won’t get cardiovascular disease.”.</p>'},
+  ]
   
+  /* define test trials */
+  const test1 = {
+    type: jsPsychHtmlSliderResponse,
+    stimulus: () => {
+    return jsPsych.evaluateTimelineVariable('stimulus1') + " " + jsPsych.evaluateTimelineVariable('prompt1');
+    },          
+    labels: ['no', 'unsure','yes'],
+    slider_width: 500,
+    require_movement: true, 
+    on_finish: function (data: TrialData) {
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, unicorn/no-null
+      data.saveIncrementally = true
+    },
+  }
+
+  /* define test procedure */
+  const test_procedure = {
+    timeline: [test1],
+    timeline_variables: test_stimuli,
+    repetitions: 1,
+    randomize_order: true,
+  }
+  timeline.push(test_procedure)
+
   /* define debrief */
   const debrief_block = {
     type: jsPsychHtmlKeyboardResponse,
@@ -167,8 +201,4 @@ var ignorance_2 = {
     },
   }
   timeline.push(debrief_block)
-
-  /* start the experiment */
-  // @ts-expect-error allow timeline to be type jsPsych TimelineArray
-  await jsPsych.run(timeline)
 }
