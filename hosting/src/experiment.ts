20751b3758052dd4447f6343acfcc222555ff683
jckend <142367196+jckend@users.noreply.github.com>
2025-07-28 11:36:18 -0400

    Update experiment.ts

--- hosting/src/experiment.ts
+++ hosting/src/experiment.ts
@@ -132,10 +132,8 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
   const instructions = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-<p>In this experiment, a circle will appear in the center of the screen.</p>
-<p>If the circle is <span class="font-semibold text-blue-500">blue</span>, press the letter <span class="font-semibold text-blue-500">F</span> on the keyboard as fast as you can.</p>
-<p>If the circle is <span class="font-semibold text-orange-500">orange</span>, press the letter <span class="font-semibold text-orange-500">J</span> as fast as you can.</p>
-<p>Press any key to begin.</p>
+      <p>In this experiment, you will be presented with an image, and asked to evaluate a sentence based on the image presented.</p>
+      <p>Press any key to begin.</p>
     `,
     post_trial_gap: 2000,
   }
