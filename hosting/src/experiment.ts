0dee682486f1e519dd0da97d49da958c5028de3b
jckend <142367196+jckend@users.noreply.github.com>
2025-07-28 10:57:54 -0400

    Update experiment.ts

--- hosting/src/experiment.ts
+++ hosting/src/experiment.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import externalHtml from '@jspsych/plugin-external-html'
+
 import jsPsychHtmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response'
+import jsPsychImageSliderResponse from '@jspsych/plugin-image-slider-response'
+import jsPsychImageKeyboardResponse from '@jspsych/plugin-image-keyboard-response'
 import jsPsychPreload from '@jspsych/plugin-preload'
 import { initJsPsych } from 'jspsych'
 
@@ -9,6 +11,9 @@ import { saveTrialDataComplete, saveTrialDataPartial } from './lib/databaseUtils
 
 import type { SaveableDataRecord } from '../types/project'
 import type { DataCollection } from 'jspsych'
+
+import imgThrow1 from './images/throw_rock_man.png'
+import imgThrow2 from './images/throw_rock_man2.png'
 
 /* Alternatively
  * type JsPsychInstance = ReturnType<typeof initJsPsych>
@@ -59,7 +64,6 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
 
   /* initialize jsPsych */
   const jsPsych = initJsPsych({
-    show_progress_bar: true,
     on_data_update: function (trialData: TrialData) {
       if (debug) {
         console.log('jsPsych-update :: trialData ::', trialData)
@@ -109,7 +113,14 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
   /* create timeline */
   const timeline: Record<string, unknown>[] = []
 
-    /* define welcome message trial */
+  /* preload images */
+  const preload = {
+    type: jsPsychPreload,
+    images: [imgThrow1, imgThrow2],
+  }
+  timeline.push(preload)
+
+  /* define welcome message trial */
   const welcome = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: '<span class="text-xl">Welcome to the experiment. Press any key to begin.</span>',
@@ -120,14 +131,50 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
   const instructions = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-      <p>In this experiment, you will be presented with an image and asked some questions.</p>
-      <p>Press any key to begin.</p>
+<p>In this experiment, a circle will appear in the center of the screen.</p>
+<p>If the circle is <span class="font-semibold text-blue-500">blue</span>, press the letter <span class="font-semibold text-blue-500">F</span> on the keyboard as fast as you can.</p>
+<p>If the circle is <span class="font-semibold text-orange-500">orange</span>, press the letter <span class="font-semibold text-orange-500">J</span> as fast as you can.</p>
+<p>Press any key to begin.</p>
     `,
     post_trial_gap: 2000,
   }
   timeline.push(instructions)
 
+  /* define trial stimuli array for timeline variables */
+  const test_stimuli: Record<string, string>[] = [
+    { stimulus: imgThrow1, prompt: '<p>If blue man throws his rock, the <span class="font-semibold text-blue-500">vase will shatter</span>.</p>'},
+    { stimulus: imgThrow1, prompt: '<p>If blue man does not throw his rock, the <span class="font-semibold text-orange-500">vase will not shatter</span>.</p>'},
+    { stimulus: imgThrow2, prompt: '<p>If blue man throws his rock, the <span class="font-semibold text-blue-500">vase will shatter</span>.</p>'},
+    { stimulus: imgThrow2, prompt: '<p>If blue man does not throw his rock, the <span class="font-semibold text-orange-500">vase will not shatter</span>.</p>'},
+  ]
 
+  
+  /*define likert scale*/
+  var likert_scale = [ "False", "Unsure", "True" ]
+
+  /* define test trials */
+  const test1 = {
+    type: jsPsychImageSliderResponse,
+    stimulus: jsPsych.timelineVariable('stimulus') as unknown as string,
+    labels: ['0%', '50%','100%'],
+    prompt: jsPsych.timelineVariable('prompt') as unknown as string,
+    slider_width: 500,
+    on_finish: function (data: TrialData) {
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, unicorn/no-null
+      data.saveIncrementally = true
+    },
+  }
+
+
+  /* define test procedure */
+  const test_procedure = {
+    timeline: [test1],
+    timeline_variables: test_stimuli,
+    repetitions: 1,
+    randomize_order: true,
+  }
+  timeline.push(test_procedure)
+  
   /* define debrief */
   const debrief_block = {
     type: jsPsychHtmlKeyboardResponse,
@@ -137,4 +184,8 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
     },
   }
   timeline.push(debrief_block)
+
+  /* start the experiment */
+  // @ts-expect-error allow timeline to be type jsPsych TimelineArray
+  await jsPsych.run(timeline)
 }
