ae767e395c988f175466e7f44c09340534179a26
jckend <142367196+jckend@users.noreply.github.com>
2025-10-01 14:26:21 -0400

    Update experiment.ts

--- hosting/src/experiment.ts
+++ hosting/src/experiment.ts
@@ -199,7 +199,6 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
     prompt: jsPsych.timelineVariable('prompt') as unknown as string,
     slider_width: 500,
     require_movement: true,
-    require_movement: true, 
     on_finish: function (data: TrialData) {
       // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, unicorn/no-null
       data.saveIncrementally = true
